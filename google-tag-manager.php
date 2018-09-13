c051e9cfbaca7c340ccfe04f7a89cec9a5dd3459
Jaason <jaason@go2.pl>
2018-09-13 11:18:44 +0200

    Remove unnecessary script tag
    According to Grav Documentation <script> tag here is not necessary: https://learn.getgrav.org/themes/asset-manager
    Also wrong comments in JS section.

--- google-tag-manager.php
+++ google-tag-manager.php
@@ -45,22 +45,22 @@ class GoogleTagManagerPlugin extends Plugin
     /**
      * Generate GTM Code for embeded.
      * @param string $trackingId Google Tag Manager Tracking ID
-     */  
+     */
     private function generateTrackingCode($trackingId) {
         $code = <<<EOF
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{$trackingId}');</script>
-<!-- End Google Tag Manager -->
+})(window,document,'script','dataLayer','{$trackingId}');
+// End Google Tag Manager
 EOF;
         return $code;
     }
     /**
      * Check if it is the right domain to apply.
-     */      
+     */
     private function isTheRightDomain() {
 
         $domains = $this->grav['config']->get('plugins.google-tag-manager.domains', []);
@@ -71,6 +71,6 @@ EOF;
         // if set, only match domain will be applied.
         if (in_array($_SERVER['SERVER_NAME'], $domains)) return True;
 
-        return False;     
+        return False;
     }
 }
